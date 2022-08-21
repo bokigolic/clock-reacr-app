ce1806ce89fdec42ee5bd7260c7c17e7ee3790f1
Bojan Golic <bokigolic32@gmail.com>
2022-08-21 01:26:50 -0400

    add css, work on state and funcionality

--- src/components/Clock.jsx
+++ src/components/Clock.jsx
@@ -1,13 +1,13 @@
 const Clock = () => {
+  
   return (
     <div class="analog-clock">
       <div className="krug">
-        <div className="pointer">
-
+        <div className="pointer-holder">
+          <div className="pointer">
+          </div>
         </div>
-
       </div>
-
     </div>
   )
 };
