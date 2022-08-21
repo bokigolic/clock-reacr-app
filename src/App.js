8f981eb97c8689406347d2a61bd2be308dcd2d14
Bojan Golic <bokigolic32@gmail.com>
2022-08-21 00:19:54 -0400

    add circle element and pointer and work on css:

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
+import Clock from "./components/Clock";
 
 
 function App() {
   return (
     <div className="App">
+      <Clock />
     </div>
   );
 }
---
+++ src/components/Clock.jsx
@@ -0,0 +1,15 @@
+const Clock = () => {
+  return (
+    <div class="analog-clock">
+      <div className="krug">
+        <div className="pointer">
+
+        </div>
+
+      </div>
+
+    </div>
+  )
+};
+
+export default Clock;
