1d3b6c969c47b3914a731fe073c3bf9db6bdcc9c
Bojan Golic <bokigolic32@gmail.com>
2022-08-22 00:21:16 -0400

    analog clock designed

--- src/components/Clock.jsx
+++ src/components/Clock.jsx
@@ -1,9 +1,18 @@
+import { useState } from "react";
+
 const Clock = () => {
-  
+  const [seconds, setSeconds] = useState(50);
+  const angle = parseInt((seconds / 60) * 360);
+
+
   return (
     <div class="analog-clock">
       <div className="krug">
-        <div className="pointer-holder">
+        <div className="pointer-holder"
+          style={{
+            transform: 'rotate(' + angle + 'deg)'
+          }}
+          >
           <div className="pointer">
           </div>
         </div>
